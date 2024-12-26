fed07f5680edefce55c6b5e2d6262504bfa2c1ff
hewenyu <yuebanlaosiji@outlook.com>
2024-12-26 14:21:21 +0800

    翻译文本

---
+++ YinYue_zh_CN.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <source>音乐播放器</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>00:00</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>上一首</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>播放</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>下一首</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>音量</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>清空播放列表</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>移除选中</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>打开文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>打开文件夹</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>退出</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>文件夹不存在</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>未知歌曲</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>未知艺术家</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>歌词文件格式错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>暂无歌词</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>打开音乐文件</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>音乐文件 (*.mp3 *.wav *.flac)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>打开音乐文件夹</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>暂停</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
